474abbeee686908853951955ce0bfd441525a153
karinecustodio <110794931+karinecustodio@users.noreply.github.com>
2024-02-21 16:12:20 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,74 @@
+export default [
+    {
+        question: "Que dia nos conhecemos?",
+        answers: [
+            {option: "", correct: },
+            {option: "", correct: },
+            {option: "", correct: },
+        ],
+    },
+    {
+        question: "",
+        answers: [
+            {option: "", correct: },
+            {option: "", correct: },
+            {option: "", correct: },
+        ],
+    },
+    {
+        question: "",
+        answers: [
+            {option: "", correct: },
+            {option: "", correct: },
+            {option: "", correct: },
+        ],
+    },
+    {
+        question: "",
+        answers: [
+            {option: "", correct: },
+            {option: "", correct: },
+            {option: "", correct: },
+        ],
+    },
+    {
+        question: "",
+        answers: [
+            {option: "", correct: },
+            {option: "", correct: },
+            {option: "", correct: },
+        ],
+    },
+    {
+        question: "",
+        answers: [
+            {option: "", correct: },
+            {option: "", correct: },
+            {option: "", correct: },
+        ],
+    },
+    {
+        question: "",
+        answers: [
+            {option: "", correct: },
+            {option: "", correct: },
+            {option: "", correct: },
+        ],
+    },
+    {
+        question: "",
+        answers: [
+            {option: "", correct: },
+            {option: "", correct: },
+            {option: "", correct: },
+        ],
+    },
+    {
+        question: "",
+        answers: [
+            {option: "", correct: },
+            {option: "", correct: },
+            {option: "", correct: },
+        ],
+    },
+]
